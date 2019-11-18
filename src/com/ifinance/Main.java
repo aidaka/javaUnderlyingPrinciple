ba4e3c1adc5aa98a6bb4b5314131b8fc47880767
aidaka <15143038938@163.com>
2019-11-18 14:51:41 +0800

    yes

---
+++ src/com/ifinance/Main.java
@@ -0,0 +1,8 @@
+package com.ifinance;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/ifinance/testGit.java
@@ -0,0 +1,8 @@
+package com.ifinance;
+
+/**
+ * @author LiuDaKa
+ * @create 2019-11-18 14:30
+ */
+public class testGit {
+}
